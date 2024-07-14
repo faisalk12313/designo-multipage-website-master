5481bd0908fae7c7ee1c91e5a212e52ef0c53b15
Faisal <faisalshabbir_khan@icloud.com>
2024-07-14 20:38:44 +0500

    delete directory

--- designo-multipage-website-master/src/store/actions/ServiceAction.js
+++
@@ -1,6 +0,0 @@
-export const setService = (data) => {
-  return {
-    type: "SET_SERVICE",
-    payload: data,
-  };
-};
--- designo-multipage-website-master/src/store/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import serviceReducer from "./ServiceReducer";
-
-const rootReducer = combineReducers({
-  service: serviceReducer,
-});
-
-export default rootReducer;
